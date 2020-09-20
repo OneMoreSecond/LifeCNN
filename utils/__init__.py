07aaa0afb1738f297fda107f7ebdd34ca7cdfe50
Tang Xianghao <t-x.h@163.com>
2020-09-20 20:27:30 +0800

    Add the eval function

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .io_helper import *
+from .eval import *
---
+++ utils/eval.py
@@ -0,0 +1,24 @@
+from fractions import Fraction
+import pdb
+
+import torch
+from torch.utils.data import DataLoader
+
+def eval(model, test_data):
+    model.eval()
+    total_count = 0
+    correct_count = 0
+
+    with torch.no_grad():
+        for inputs, expected_outputs in test_data:
+            assert inputs.shape == expected_outputs.shape, f'inputs:{inputs.shape} outputs:{expected_outputs}'
+            batch_size = expected_outputs.shape[0]
+            total_count += batch_size
+
+            logits = model(inputs.type(torch.get_default_dtype()))
+            actual_outputs = logits > 0
+            assert actual_outputs.shape == expected_outputs.shape, f'expected:{expected_outputs.shape} actual:{actual_outputs.shape}'
+            is_same = (expected_outputs == actual_outputs).view(batch_size, -1).all(1)
+            correct_count += is_same.sum(dtype=torch.int).item()
+
+    return Fraction(correct_count, total_count)
