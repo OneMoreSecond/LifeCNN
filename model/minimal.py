dfc8fae1d0e25e6a5f691f9ad64b13a7d67524c7
Tang Xianghao <t-x.h@163.com>
2020-09-20 20:26:59 +0800

    Add the minimal model

---
+++ model/minimal.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MinimalModel(nn.Module):
+    def __init__(self, step=1, channel_scale=1, initialization='random', **kwargs):
+        super().__init__()
+
+        self.step = step
+        self.conv1 = nn.Conv2d(1, 2 * channel_scale, 3, padding=1)
+        self.conv2 = nn.Conv2d(2 * channel_scale, 1 * channel_scale, 1)
+        self.output_conv = nn.Conv2d(1 * channel_scale, 1, 1)
+
+        if initialization == 'ground_truth':
+            assert channel_scale == 1, 'ground truth weight initialization does not support channel scale'
+
+            with torch.no_grad():
+                assert self.conv1.weight.shape == (2, 1, 3, 3)
+                self.conv1.weight.fill_(1.0)
+                self.conv1.weight[0, 0, 1, 1] = 0.1
+
+                assert self.conv1.bias.shape == (2,)
+                self.conv1.bias[0] = -3.0
+                self.conv1.bias[1] = -2.0
+
+                assert self.conv2.weight.shape == (1, 2, 1, 1)
+                self.conv2.weight[0, 0, 0, 0] = -10.0
+                self.conv2.weight[0, 1, 0, 0] = 1.0
+
+                self.conv2.bias.zero_()
+
+                assert self.output_conv.weight.shape == (1, 1, 1, 1)
+                self.output_conv.weight[0, 0, 0, 0] = 40.0
+
+                assert self.output_conv.bias.shape == (1,)
+                self.output_conv.bias[0] = -20.0
+
+    def forward(self, x):
+        for i in range(self.step):
+            x = self.conv1(x)
+            x = F.relu(x)
+            x = self.conv2(x)
+            x = F.relu(x)
+
+        x = self.output_conv(x)
+        return x
