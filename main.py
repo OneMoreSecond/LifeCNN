6e4418bc5444c09d614da51d9bd269a1014585d0
Tang Xianghao <t-x.h@163.com>
2020-09-20 22:43:13 +0800

    support training in main.py

--- main.py
+++ main.py
@@ -4,22 +4,33 @@ import argparse
 
 import data
 import model
+import utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument('mode', choices=['train', 'test'])
 parser.add_argument('model_dir', help='the directory storing models')
 parser.add_argument('--model_config', help='if not specified, use model_dir/model_config.json')
-parser.add_argument('--train_config', default='config/default/train_config.json')
-parser.add_argument('--test_config', default='config/default/test_config.json')
+parser.add_argument('--train_config', help='if not specified, use model_dir/train_config.json')
+parser.add_argument('--eval_config', default='config/default/eval_config.json')
 
 args = parser.parse_args()
 
 model_config_path = os.path.join(args.model_dir, 'model_config.json')
 if args.model_config_path is not None:
     model_config_path = args.model_config_path
+model_config = utils.load_json(model_config_path)
+game_step = model_config['game_step']
+
+eval_config = utils.load_json(args.eval_config)
 
 if args.mode == 'train':
-    pass
+    train_config = utils.load_json(args.train_config)
+    the_model = model.MinimalModel(**model_config)
+    train_data = data.RandomDataset(game_step, **train_config['data'])
+    valid_datasets = [ ('random_data', data.RandomDataset(game_step, **eval_config)) ]
+    if game_step == 1:
+        valid_datasets.append( ('minimal_data', data.MinimalDataset(game_step)) )
+    utils.fit(the_model, train_data, valid_datasets, **train_config['schedule'])
 elif args.mode == 'test':
     pass
 else:
