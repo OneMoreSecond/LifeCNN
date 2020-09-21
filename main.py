dacf785fcb71c80d83e12364af6d4013ac3c7144
Xianghao Tang <xianta@microsoft.com>
2020-09-21 13:27:34 +0800

    Fix issues in training

--- main.py
+++ main.py
@@ -16,21 +16,27 @@ parser.add_argument('--eval_config', default='config/default/eval_config.json')
 args = parser.parse_args()
 
 model_config_path = os.path.join(args.model_dir, 'model_config.json')
-if args.model_config_path is not None:
-    model_config_path = args.model_config_path
+if args.model_config is not None:
+    model_config_path = args.model_config
 model_config = utils.load_json(model_config_path)
 game_step = model_config['game_step']
 
 eval_config = utils.load_json(args.eval_config)
 
 if args.mode == 'train':
-    train_config = utils.load_json(args.train_config)
+    train_config_path = os.path.join(args.model_dir, 'train_config.json')
+    if args.train_config is not None:
+        train_config_path = args.train_config
+    train_config = utils.load_json(train_config_path)
+
     the_model = model.MinimalModel(**model_config)
     train_data = data.RandomDataset(game_step, **train_config['data'])
     valid_datasets = [ ('random_data', data.RandomDataset(game_step, **eval_config)) ]
     if game_step == 1:
-        valid_datasets.append( ('minimal_data', data.MinimalDataset(game_step)) )
+        valid_datasets.append( ('minimal_data', data.MinimalDataset()) )
+
     utils.fit(the_model, train_data, valid_datasets, **train_config['schedule'])
+
 elif args.mode == 'test':
     pass
 else:
--- utils/train.py
+++ utils/train.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 
 from .eval import eval
 
-def fit(model, train_data, valid_datasets, early_stop=None, disp_freq=100, valid_freq=1000):
+def fit(model, train_data, valid_datasets, early_stop=None, display_freq=100, valid_freq=1000):
     optimizer = optim.Adam(model.parameters())
     loss_func = nn.BCEWithLogitsLoss()
 
@@ -21,18 +21,19 @@ def fit(model, train_data, valid_datasets, early_stop=None, disp_freq=100, valid
         logits = model(train_inputs.type(torch.get_default_dtype()))
         assert train_outputs.shape == logits.shape, f'expected:{train_outputs.shape} actual:{logits.shape}'
 
-        loss = loss_func(logits, train_outputs)
+        loss = loss_func(logits, train_outputs.type_as(logits))
         loss.backward()
         optimizer.step()
 
-        if (train_step + 1) % disp_freq == 0:
-            print(f'{datetime.now()} step {train_step} loss {loss.item()}')
+        step_count = train_step + 1
+        if step_count % display_freq == 0:
+            print(f'{datetime.now()} step {step_count} loss {loss.item()}')
 
-        if len(valid_datasets) > 0 and (train_step + 1) % valid_freq == 0:
-            print(f'Validation on step {i}...')
+        if len(valid_datasets) > 0 and (step_count) % valid_freq == 0:
+            print(f'Validation on step {step_count}...')
             for i, (name, valid_data) in enumerate(valid_datasets):
                 accuracy = eval(model, valid_data)
-                print('Accuracy on {name} is {accuracy}')
+                print(f'Accuracy on {name} is {accuracy}')
 
                 if i == 0:
                     if accuracy > best_valid_accuracy:
@@ -41,8 +42,6 @@ def fit(model, train_data, valid_datasets, early_stop=None, disp_freq=100, valid
                         stalled_step = 0
                     else:
                         stalled_step += 1
-                        print('Stalled for {stalled_step} steps')
-                        if stalled_step == early_stop:
-                            break
-            else:
+                        print(f'Stalled for {stalled_step} steps')
+            if stalled_step == early_stop:
                 break
