255ce1687948bd7be35a9142baa124f2c8a74ee5
Xianghao Tang <xianta@microsoft.com>
2020-09-21 14:21:08 +0800

    Add a input convolution layer in the minimal model

--- model/minimal.py
+++ model/minimal.py
@@ -6,15 +6,25 @@ class MinimalModel(nn.Module):
     def __init__(self, game_step=1, channel_scale=1, initialization='random'):
         super().__init__()
 
-        self.step = game_step
-        self.conv1 = nn.Conv2d(1, 2 * channel_scale, 3, padding=1)
-        self.conv2 = nn.Conv2d(2 * channel_scale, 1, 1)
-        self.output_conv = nn.Conv2d(1, 1, 1)
+        self.game_step = game_step
+
+        if game_step != 1 and channel_scale != 1:
+            self.input_conv = nn.Conv2d(1, 1 * channel_scale, 1)
+        else:
+            self.input_conv = None
+
+        self.conv1 = nn.Conv2d(1 * channel_scale, 2 * channel_scale, 3, padding=1)
+        self.conv2 = nn.Conv2d(2 * channel_scale, 1 * channel_scale, 1)
+        self.output_conv = nn.Conv2d(1 * channel_scale, 1, 1)
 
         if initialization == 'ground_truth':
             assert channel_scale == 1, 'ground truth weight initialization does not support channel scale'
 
             with torch.no_grad():
+                if self.input_conv is not None:
+                    self.input_conv.weight.fill_(1.0)
+                    self.input_conv.bias.fill_(0.0)
+
                 assert self.conv1.weight.shape == (2, 1, 3, 3)
                 self.conv1.weight.fill_(1.0)
                 self.conv1.weight[0, 0, 1, 1] = 0.1
@@ -36,7 +46,10 @@ class MinimalModel(nn.Module):
                 self.output_conv.bias[0] = -20.0
 
     def forward(self, x):
-        for i in range(self.step):
+        if self.input_conv is not None:
+            x = self.input_conv(x)
+
+        for i in range(self.game_step):
             x = self.conv1(x)
             x = F.relu(x)
             x = self.conv2(x)
